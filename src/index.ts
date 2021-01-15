f9327152797aa83419a180b3150bd05624890cc8
brendan rooney <brendan.rooney@brendans-Mac-mini.local>
2021-01-15 14:48:28 +0000

    initial

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import http from 'http';
+
+require('dotenv').config();
+
+const app = express();
+const server = new http.Server(app);
+server.listen(process.env.PORT);
+
+import getAllUsersController from './controllers/getAllUsersController';
+app.get('');
